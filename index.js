3905ed44f265716cf909c21ed65211b6607be22c
Paul Goux <paul.goux@wanadoo.fr>
2022-01-08 19:02:11 +0100

    Add manifest & first implementation using theme

---
+++ index.js
@@ -0,0 +1,21 @@
+async function main() {
+    const { tabs: tabsAPI, theme: themeAPI } = browser;
+    const url = "https://github.com/cfinke/True-Colors/blob/master/true-colors/chrome/content/overlay.js"
+
+    const defaultTheme = await themeAPI.getCurrent(1);
+
+    tabsAPI.onActivated.addListener(async ({ tabId, windowId }) => {
+        const activeTab = await tabsAPI.get(tabId)
+        if (activeTab.url === url) {
+            let newTheme = structuredClone(defaultTheme)
+            newTheme.colors.tab_selected = "rgb(151, 34, 34)"
+            themeAPI.update(windowId, newTheme)
+        } else {
+            // TODO update only if previous had a specific theme+
+            themeAPI.update(windowId, defaultTheme)
+        }
+
+    })
+}
+
+main()
