d39728720f39c0eba28272a18c70b01858ccc452
Paul Goux <paul.goux@wanadoo.fr>
2022-03-27 17:49:14 +0200

    Fix bug & avodi global var

--- background.js
+++ background.js
@@ -1,6 +1,4 @@
 const { tabs: tabsAPI, theme: themeAPI, storage: storageAPI } = browser;
-let defaultTheme = null;
-let options = null;
 
 function isURLMatching(colorPreference, url) {
     if (colorPreference.isRegex) {
@@ -9,30 +7,30 @@ function isURLMatching(colorPreference, url) {
     return colorPreference.url === url;
 }
 
-async function onTabChanged({ tabId, windowId }) {
-    const activeTab = await tabsAPI.get(tabId)
-    colorPreference = options.colorPreferences.find(cp => isURLMatching(cp, activeTab.url))
-
-    if (colorPreference) {
-        let newTheme = structuredClone(defaultTheme)
-        newTheme.colors.tab_selected = "rgb(151, 34, 34)"
-        themeAPI.update(windowId, newTheme)
-    } else {
-        // TODO update only if previous had a specific theme+
-        themeAPI.update(windowId, defaultTheme)
-    }
-}
-
 async function main() {
-    defaultTheme = await themeAPI.getCurrent(1);
-    options = (await storageAPI.local.get('colorPreferences'))
-    console.log(options)
+    let defaultTheme = await themeAPI.getCurrent(1);
+    let options = await storageAPI.local.get('colorPreferences');
+
+    async function onTabChanged({ tabId, windowId }) {
+        const activeTab = await tabsAPI.get(tabId)
+        colorPreference = options.colorPreferences.find(cp => isURLMatching(cp, activeTab.url))
+
+        if (colorPreference) {
+            let newTheme = structuredClone(defaultTheme)
+            newTheme.colors.tab_selected = colorPreference.color
+            themeAPI.update(windowId, newTheme)
+        } else {
+            // TODO update only if previous had a specific theme+
+            themeAPI.update(windowId, defaultTheme)
+        }
+    }
+
+    storageAPI.onChanged.addListener((changes) => {
+        options = Object.fromEntries(Object.entries(changes).map(([key, values]) => [key, values.newValue]))
+    })
+
     tabsAPI.onActivated.addListener(onTabChanged)
     await browser.runtime.openOptionsPage()
 }
-
-storageAPI.onChanged.addListener((changes) => {
-    options = Object.fromEntries(Object.entries(changes).map(([key, values]) => [key, values.newValue]))
-})
 
 main()
